3199650bd3292ae72a30fcebf2ab4ff7ec5cab10
gilmae <avocadia@fastmail.fm>
2020-12-26 16:27:10 +1100

    Completed chapter 1

---
+++ token/token.go
@@ -0,0 +1,66 @@
+package token
+
+// TokenType defines what type a Token is
+type TokenType string
+
+// Token defines an AST toklen
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN  = "="
+	PLUS    = "+"
+	MINUS   = "-"
+	ASTERIX = "*"
+	FSLASH  = "/"
+
+	BANG = "!"
+
+	EQ    = "=="
+	NOTEQ = "!="
+
+	GT = "GT"
+	LT = "LT"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"return": RETURN,
+	"if":     IF,
+	"else":   ELSE,
+}
+
+// LookupIdent checks if an identifier is a keyword or a user identifier
+func LookupIdent(ident string) TokenType {
+	if tok, found := keywords[ident]; found {
+		return tok
+	}
+	return IDENT
+}
