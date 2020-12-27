438971ff9ce589e2b91b80cf6ba9339c8c9fbe22
gilmae <avocadia@fastmail.fm>
2020-12-27 15:21:28 +1100

    Parse return statements

--- parser/parser.go
+++ parser/parser.go
@@ -53,6 +53,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -72,6 +74,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	// TODO Expression will be here
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO parse expressions
 
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
