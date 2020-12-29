c609f2c6721f2f9a8dac5108027dd89840b88884
gilmae <avocadia@fastmail.fm>
2020-12-30 10:33:26 +1100

    Evalue minus prefix

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -42,10 +42,21 @@ func evalBangOperatorExpression(obj object.Object) object.Object {
 	}
 }
 
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
 func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return nil
 	}
